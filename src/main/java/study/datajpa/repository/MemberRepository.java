f39815532dd2fcf5b817382d81a58e60327a8896
juwonju <jxwxnjx@gmail.com>
2023-04-25 19:15:13 +0900

    Projections

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -62,4 +62,7 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Member> findLockByUsername(String name);
+
+    <T> List<T> findProjectionsByUsername(String username, Class<T> type);
+
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -306,5 +306,29 @@ class MemberRepositoryTest {
         // then
         Assertions.assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void projections() throws Exception {
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+        em.flush();
+        em.clear();
+
+        // when
+        List<NestedClosedProjection> result = memberRepository.findProjectionsByUsername("m1", NestedClosedProjection.class);
+
+        for (NestedClosedProjection nestedClosedProjection : result) {
+            System.out.println("nestedClosedProjection = " + nestedClosedProjection);
+        }
+
+        // then
+        assertThat(result.size()).isEqualTo(1);
+    }
+
     
 }
